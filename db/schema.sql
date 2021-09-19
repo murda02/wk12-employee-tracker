809304f5e5ada04fda4a2f5f63e909e1b4f4f9ba
Dave Murphy <dave.murphy@plusonerobotics.com>
2021-09-19 14:27:09 -0600

    added seeds for db

--- db/schema.sql
+++ db/schema.sql
@@ -10,18 +10,15 @@ CREATE TABLE department (
 
 CREATE TABLE empl_role (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(30),
+    title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
-    -- FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
-    department_id FOREIGN KEY REFERENCES department(id) ON DELETE SET NULL
+    department_id FOREIGN KEY REFERENCES department(id) ON DELETE CASCADE
 );
 
 CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(30),
-    last_name VARCHAR(30),
-    empl_role_id FOREIGN KEY REFERENCES empl_role(id) ON DELETE SET NULL,
-    -- not sure how to make manager_id conditional (i.e if employee has none)
-    -- this is a start...
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    empl_role_id FOREIGN KEY REFERENCES empl_role(id) ON DELETE CASCADE,
     manager_id FOREIGN KEY REFERENCES employee(id) ON DELETE SET NULL
 );
